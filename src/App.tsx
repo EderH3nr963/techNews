cc0ec5b17f80a11a3c34db9ad23f179683f1546c
Hello World <ederhenriquevicentejust18@gmail.com>
2025-04-26 22:54:35 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import LoaddingComponent from "./components/loaddingComponent";
+
+const NewsList = () => {
+  const [news, setNews] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const apiKey = import.meta.env.VITE_NEWS_API_KEY;
+
+  useEffect(() => {
+    console.log("passei port aqui");
+    axios
+      .get(
+        `https://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=${apiKey}`
+      )
+      .then((response) => {
+        setNews(response.data.articles);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Erro ao buscar as notícias", error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <LoaddingComponent />;
+  }
+
+  return (
+    <div className="container mx-auto p-5">
+      <div className="flex flex-col gap-10 mb-5">
+        <h1 className="text-3xl font-bold text-left before:w-full before:h-1 before:bg-blue-500 before:block after:h-1 after:bg-blue-500 after:block">Tech News</h1>
+        <h2 className="text-2xl font-bold">Today News</h2>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+        {news.map((article: any, index: number) => (
+          <div key={index} className="border rounded-lg p-4 shadow-md hover:scale-105 duration-300">
+            <img
+              src={article.urlToImage}
+              alt={article.title}
+              className="w-full h-48 object-cover rounded-lg mb-4"
+            />
+            <h2 className="font-semibold text-lg">{article.title}</h2>
+            <p className="text-sm text-gray-600 mb-4">{article.description}</p>
+            <a
+              href={article.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-500 hover:underline"
+            >
+              Leia mais
+            </a>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default NewsList;
---
+++ src/components/loaddingComponent.tsx
@@ -0,0 +1,7 @@
+export default function LoaddingComponent() {
+    return (
+        <div className="flex items-center justify-center h-[100vh]">
+            <div className="w-20 h-20 animate-spin rounded-full border-8 border-transparent border-b-black" ></div>
+        </div>
+    );
+}
